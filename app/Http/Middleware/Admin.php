fc4630037abd5c331bca440e7282d64e36ea5829
guillaume <guillaume.tatuszka@gmail.com>
2018-04-10 14:40:29 +0200

    creation admin, et finalisation du back de resultat

---
+++ app/Http/Middleware/Admin.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class Admin
+{
+
+    public function handle($request, Closure $next)
+    {
+        $user = $request->user();
+        if ($user && $user->role === 'admin') {
+            return $next($request);
+        }
+        return redirect()->route('home');
+    }
+}
--- resources/views/resultat.blade.php
+++ resources/views/resultat.blade.php
@@ -2,8 +2,11 @@
 
 @section('CSSpages')
 <link href='css/pronostics.css' rel='stylesheet'>
-@endsection @section('content')
+@endsection
 
+@section('content')
+
+	@admin
 <!-- MATCH 1 -->
 <div class="container">
 	<div class="Match">
@@ -14,143 +17,53 @@
 			</div>
 			<div class="col-md-2 col-xs-2"></div>
 		</div>
+
 		<div class="equipes row ">
-			<div class="equipe1 col-md-2 col-xs-16">
-				<p>
-					<br />Equipe 1<br />
-				</p>
-			</div>
-			<p class="versus">VS</p>
-			<div class="equipe2 col-md-2 col-xs-16">
-				<p>
-					<br />Equipe 2<br />
-				</p>
-			</div>
-
-			<div class="scorePrev col-md-4">
-				<p>Score prévisionel</p>
-			</div>
-			<div class="equipeVotes col-md-2">
-				<p>equipe 1</p>
-			</div>
-			<div class="equipeVotes col-md-2">
-				<p>equipe 2</p>
-			</div>
-			<div class="scores">
-				<div class="score1 col-xs-2">
-					<label class="sr-only" for="inlineFormInputName2">Score1</label> <input
-						type="number" class="form-control mb-2 mr-sm-2"
-						id="inlineFormInputName2" placeholder="score-1" disabled="disabled" >
+			<form action="{{ route('resultat.store') }}" method="post">
+				{{ csrf_field() }}
+				<div class="equipe1 col-md-2 col-xs-16">
+					<p>
+						<br /><input type="text" name="equipe_1"><br />
+					</p>
 				</div>
-				<div class="score2 col-xs-2">
-					<label class="sr-only" for="inlineFormInputName2">Score2</label> <input
-						type="number" class="form-control mb-2 mr-sm-2"
-						id="inlineFormInputName2" placeholder="score-2" disabled="disabled" >
+				<p class="versus">VS</p>
+				<div class="equipe2 col-md-2 col-xs-16">
+					<p>
+						<br /><input type="text" name="equipe_2"><br />
+					</p>
 				</div>
-				
-			</div>
 
-
-		</div>
-	</div>
-
-	<!-- MATCH 2 -->
-
-	<div class="Match">
-		<div class="row">
-			<div class="col-md-2 col-xs-2"></div>
-			<div class="match col-md-8 col-xs-12">
-				<p>Match du "date" :</p>
-			</div>
-			<div class="col-md-2 col-xs-2"></div>
-		</div>
-		<div class="equipes row ">
-			<div class="equipe1 col-md-2 col-xs-16">
-				<p>
-					<br />Equipe 1<br />
-				</p>
-			</div>
-			<p class="versus">VS</p>
-			<div class="equipe2 col-md-2 col-xs-16">
-				<p>
-					<br />Equipe 2<br />
-				</p>
-			</div>
-
-			<div class="scorePrev col-md-4">
-				<p>Score prévisionel</p>
-			</div>
-			<div class="equipeVotes col-md-2">
-				<p>equipe 1</p>
-			</div>
-			<div class="equipeVotes col-md-2">
-				<p>equipe 2</p>
-			</div>
-			<div class="scores">
-				<div class="score1 col-xs-2">
-					<label class="sr-only" for="inlineFormInputName2">Score1</label> <input
-						type="number" class="form-control mb-2 mr-sm-2"
-						id="inlineFormInputName2" placeholder="score-1" disabled="disabled" >
+				<div class="scorePrev col-md-4">
+					<p>Résultat du match</p>
 				</div>
-				<div class="score2 col-xs-2">
-					<label class="sr-only" for="inlineFormInputName2">Score2</label> <input
-						type="number" class="form-control mb-2 mr-sm-2"
-						id="inlineFormInputName2" placeholder="score-2" disabled="disabled" >
+				<div class="equipeVotes col-md-2">
+					<p>equipe 1</p>
 				</div>
-			</div>
-			
-		</div>
-	</div>
-	<!-- MATCH 3 -->
-	<div class="Match">
-		<div class="row">
-			<div class="col-md-2 col-xs-2"></div>
-			<div class="match col-md-8 col-xs-12">
-				<p>Match du "date" :</p>
-			</div>
-			<div class="col-md-2 col-xs-2"></div>
-		</div>
-		<div class="equipes row ">
-			<div class="equipe1 col-md-2 col-xs-16">
-				<p>
-					<br />Equipe 1<br />
-				</p>
-			</div>
-			<p class="versus">VS</p>
-			<div class="equipe2 col-md-2 col-xs-16">
-				<p>
-					<br />Equipe 2<br />
-				</p>
-			</div>
-
-			<div class="scorePrev col-md-4">
-				<p>Score prévisionel</p>
-			</div>
-			<div class="equipeVotes col-md-2">
-				<p>equipe 1</p>
-			</div>
-			<div class="equipeVotes col-md-2">
-				<p>equipe 2</p>
-			</div>
-			<div class="scores">
-				<div class="score1 col-xs-2">
-					<label class="sr-only" for="inlineFormInputName2">Score1</label> <input
-						type="number" class="form-control mb-2 mr-sm-2"
-						id="inlineFormInputName2" placeholder="score-1" disabled="disabled" >
+				<div class="equipeVotes col-md-2">
+					<p>equipe 2</p>
 				</div>
-				<div class="score2 col-xs-2">
-					<label class="sr-only" for="inlineFormInputName2">Score2</label> <input
-						type="number" class="form-control mb-2 mr-sm-2"
-						id="disabledInput" placeholder="score-2" disabled="disabled" >
+				<div class="scores">
+					<div class="score1 col-xs-2">
+						<label class="sr-only" for="inlineFormInputName2">Score1</label>
+						<input type="number" name="score_1" class="form-control mb-2 mr-sm-2"
+							id="inlineFormInputName2" placeholder="score_1">
+					</div>
+					<div class="score2 col-xs-2">
+						<label class="sr-only" for="inlineFormInputName2">Score2</label>
+						<input type="number" name="score_2" class="form-control mb-2 mr-sm-2"
+							id="inlineFormInputName2" placeholder="score_2">
+						<input type="submit" class="buton btn btn-secondary btn-sm">valider</input>
+					</div>
 				</div>
-				
-			</div>
-			
+			</form>
 		</div>
 	</div>
 </div>
+	@else
 
-<!-- Saut d'une ligne a la fin -->
-<br>
+	<h1>Vous n'etes pas admin pd</h1>
+
+	@endadmin
+
 @endsection
 
