191ebc71ed93d4461683bc51eaba397cba70446b
Seb <allimant.sebastien@gmail.com>
2018-04-10 14:49:40 +0200

    rebase???

--- resources/views/resultat.blade.php
+++ resources/views/resultat.blade.php
@@ -18,6 +18,7 @@
 			<div class="col-md-2 col-xs-2"></div>
 		</div>
 
+<<<<<<< 511c50a93523ece722d34616e7e53bc421f28f1a
 		<div class="equipes row ">
 			<form action="{{ route('resultat.store') }}" method="post">
 				{{ csrf_field() }}
@@ -25,6 +26,22 @@
 					<p>
 						<br /><input type="text" name="equipe_1"><br />
 					</p>
+=======
+			<div class="scorePrev col-md-4">
+				<p>Score Match</p>
+			</div>
+			<div class="equipeVotes col-md-2">
+				<p>equipe 1</p>
+			</div>
+			<div class="equipeVotes col-md-2">
+				<p>equipe 2</p>
+			</div>
+			<div class="scores">
+				<div class="score1 col-xs-2">
+					<label class="sr-only" for="inlineFormInputName2">Score1</label> <input
+						type="number" class="form-control mb-2 mr-sm-2"
+						id="inlineFormInputName2" placeholder="score-1" disabled="disabled" >
+>>>>>>> correction score in resultat match, add model for page pronostic
 				</div>
 				<p class="versus">VS</p>
 				<div class="equipe2 col-md-2 col-xs-16">
@@ -33,14 +50,95 @@
 					</p>
 				</div>
 
+<<<<<<< 511c50a93523ece722d34616e7e53bc421f28f1a
 				<div class="scorePrev col-md-4">
 					<p>Résultat du match</p>
+=======
+	<div class="Match">
+		<div class="row">
+			<div class="col-md-2 col-xs-2"></div>
+			<div class="match col-md-8 col-xs-12">
+				<p>Match du "date" :</p>
+			</div>
+			<div class="col-md-2 col-xs-2"></div>
+		</div>
+		<div class="equipes row ">
+			<div class="equipe1 col-md-2 col-xs-16">
+				<p>
+					<br />Equipe 1<br />
+				</p>
+			</div>
+			<p class="versus">VS</p>
+			<div class="equipe2 col-md-2 col-xs-16">
+				<p>
+					<br />Equipe 2<br />
+				</p>
+			</div>
+
+			<div class="scorePrev col-md-4">
+				<p>Score Match</p>
+			</div>
+			<div class="equipeVotes col-md-2">
+				<p>equipe 1</p>
+			</div>
+			<div class="equipeVotes col-md-2">
+				<p>equipe 2</p>
+			</div>
+			<div class="scores">
+				<div class="score1 col-xs-2">
+					<label class="sr-only" for="inlineFormInputName2">Score1</label> <input
+						type="number" class="form-control mb-2 mr-sm-2"
+						id="inlineFormInputName2" placeholder="score-1" disabled="disabled" >
+>>>>>>> correction score in resultat match, add model for page pronostic
 				</div>
 				<div class="equipeVotes col-md-2">
 					<p>equipe 1</p>
 				</div>
+<<<<<<< 511c50a93523ece722d34616e7e53bc421f28f1a
 				<div class="equipeVotes col-md-2">
 					<p>equipe 2</p>
+=======
+			</div>
+			
+		</div>
+	</div>
+	<!-- MATCH 3 -->
+	<div class="Match">
+		<div class="row">
+			<div class="col-md-2 col-xs-2"></div>
+			<div class="match col-md-8 col-xs-12">
+				<p>Match du "date" :</p>
+			</div>
+			<div class="col-md-2 col-xs-2"></div>
+		</div>
+		<div class="equipes row ">
+			<div class="equipe1 col-md-2 col-xs-16">
+				<p>
+					<br />Equipe 1<br />
+				</p>
+			</div>
+			<p class="versus">VS</p>
+			<div class="equipe2 col-md-2 col-xs-16">
+				<p>
+					<br />Equipe 2<br />
+				</p>
+			</div>
+
+			<div class="scorePrev col-md-4">
+				<p>Score Match</p>
+			</div>
+			<div class="equipeVotes col-md-2">
+				<p>equipe 1</p>
+			</div>
+			<div class="equipeVotes col-md-2">
+				<p>equipe 2</p>
+			</div>
+			<div class="scores">
+				<div class="score1 col-xs-2">
+					<label class="sr-only" for="inlineFormInputName2">Score1</label> <input
+						type="number" class="form-control mb-2 mr-sm-2"
+						id="inlineFormInputName2" placeholder="score-1" disabled="disabled" >
+>>>>>>> correction score in resultat match, add model for page pronostic
 				</div>
 				<div class="scores">
 					<div class="score1 col-xs-2">
