e118fff1154019cc8ffd2001c20933d087cd0eac
Seb <allimant.sebastien@gmail.com>
2018-04-10 14:45:08 +0200

    commit

--- app/Http/Controllers/PronosticsController.php
+++ app/Http/Controllers/PronosticsController.php
@@ -7,13 +7,11 @@ use App\Pronostics;
 
 class PronosticsController extends Controller
 {
-    public function index()
-    {
+    public function index(){
         return view('pronostics');
     }
     
     public function create(){
-        $scores = Pronostics::insert('INSERT INTO pronostic (id, id_user, id_match, score_equipe_1, score_equipe_2)
-                                VALUES (?, ?, ?, ?, ?)');
+
     }
 }
