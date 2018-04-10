8cb49e751cccbade914364b383c397ff44780be3
Seb <allimant.sebastien@gmail.com>
2018-04-10 09:07:01 +0200

    add model and controller

--- app/Http/Controllers/PronosticsController.php
+++ app/Http/Controllers/PronosticsController.php
@@ -3,11 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Pronostics;
 
 class PronosticsController extends Controller
 {
     public function index()
     {
         return view('pronostics');
+    }
+    
+    public function create(){
+        $scores = Pronostics::insert('INSERT INTO pronostic (id, id_user, id_match, score_equipe_1, score_equipe_2)
+                                VALUES (?, ?, ?, ?, ?)');
     }
 }
