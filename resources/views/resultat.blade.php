39c76efafc5fdbdefb5aa05fe160178439aad167
Seb <allimant.sebastien@gmail.com>
2018-04-11 15:36:51 +0200

    resolve conflict ressource

--- resources/views/resultat.blade.php
+++ resources/views/resultat.blade.php
@@ -163,3 +163,74 @@
 	@endguest
 @endsection
 
+    @admin
+<!-- MATCH admin -->
+<div class="Match container">
+        <form action="{{ route('resultat.store') }}" method="post">
+            {{ csrf_field() }}
+        <div class="row">
+            <div class="col-md-2"></div>
+            <div class="panel panel-primary match col-xs-12 col-sm-12 col-md-8 ">
+                <div class="panel-heading"><span>Match du "date"  :</span></div>
+            </div>
+            <div class="col-md-2"></div>
+        </div>
+
+        <div class="equipes row">
+            <div class="col-md-2"></div>
+                <div class="equipe1 col-xs-12 col-sm-12 col-md-3">
+                    <input type="text" class="form-control" name="equipe_1" placeholder="Equipe 1">
+                </div>
+                <div class="versus col-xs-12 col-sm-12 col-md-2">VS</div>
+                <div class="equipe2 col-xs-12 col-sm-12 col-md-3">
+                    <input type="text" class="form-control" name="equipe_2" placeholder="Equipe 2">
+                </div>
+            <div class="col-md-2"></div>
+        </div>
+        <div class="score row">
+                <div class="result col-xs-12 col-sm-12 col-md-12">
+                    <span>Résultat du match</span>
+                </div>
+        </div>
+            <div class="row">
+                <div class="col-md-2"></div>
+
+
+                <div class="col-md-2"></div>
+            </div>
+                <div class="scores row">
+                    <div class="col-md-2"></div>
+                    <div class="score1 col-md-4">
+                        <div class="equipeVotes">
+                            <span>Equipe 1</span>
+                        </div>
+                        <label class="sr-only" for="inlineFormInputName2">Score1</label>
+                        <input type="number" name="score_1" class="form-control mb-2 mr-sm-2"
+                            id="inlineFormInputName2" placeholder="score_1">
+                    </div>
+                    <div class="score2 col-md-4">
+                        <div class="equipeVotes">
+                            <span>Equipe 2</span>
+                        </div>
+                        <label class="sr-only" for="inlineFormInputName2">Score2</label>
+                        <input type="number" name="score_2" class="form-control mb-2 mr-sm-2"
+                            id="inlineFormInputName2" placeholder="score_2">
+                    </div>
+                    <div class="col-md-2"></div>
+                </div>
+            <div class="row">
+                <div class="col-md-5"></div>
+                <div class="col-xs-offset-4 col-sm-6 col-md-2">
+                    <input type="submit" class="button btn btn-primary btn-lg">
+                </div>
+                <div class="col-md-5"></div>
+            </div>
+        </form>
+</div>
+    @else
+
+    <h1></h1>
+
+    @endadmin
+
+@endsection
